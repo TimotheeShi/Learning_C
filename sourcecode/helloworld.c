daed3b6afc0b05a2dde3afdfc01823075cd82a61
TimotheeShi <15651817889@163.com>
2024-04-25 01:39:53 +0800

    Delete some directories

--- sourcecode/helloworld.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-main()
-{
-    // printf("hello, world\n");
-    // printf("hello");
-    printf("world");
-    printf("\n"); // if there is no `\n` in the printf, there will not have output in the terminal.
-}
